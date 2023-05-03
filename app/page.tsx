245cfbb577707746b6505158e2f78ee231af3f91
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-05-03 16:41:48 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Trending from "./(shared)/Trending"
+
 export default function Home() {
-  return <main>hi</main>
+  return (
+    <main className="px-10 leading-7">
+      <Trending />
+      <div className="md:flex gap-10 mb-5">
+        <div className="basis-3/4">
+          <div className="hidden md:block"></div>
+        </div>
+        <div className="basis-1/4"></div>
+      </div>
+    </main>
+  )
 }
