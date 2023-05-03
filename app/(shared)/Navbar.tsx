ef9f66a4f2ae48e2d0b8453e9c82f57267df4dbb
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-05-03 15:23:40 +0900

    add header

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -2,13 +2,17 @@ import React from "react"
 import Link from "next/link"
 import Image from "next/image"
 
+import SocialLinks from "./SocialLinks"
+
 type Props = {}
 
 const Navbar = (props: Props) => {
   return (
     <header className="mb-5">
       <nav className="flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4">
-        <div className="hidden sm:block">social link</div>
+        <div className="hidden sm:block">
+          <SocialLinks />
+        </div>
         <div className="flex justify-between items-center gap-10">
           <Link href="/">Home</Link>
           <Link href="/">Trending</Link>
